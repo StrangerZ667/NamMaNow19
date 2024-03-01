dabe449281680d32ff1977fd15543b0dfbc4e72c
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-03-01 14:29:33 +0700

    Lab24_2 Updated

--- lab24_2.cpp
+++ lab24_2.cpp
@@ -15,7 +15,7 @@ void printList(list<string> s){
 int main(){
 	
 	list<string> line_up;
-	list<string>::iterator loc;
+	list<string>::iterator loc,loc1,loc2,loc3;
 	
 	line_up.push_back("Alice");
 	line_up.push_back("Bob");
@@ -23,7 +23,27 @@ int main(){
 	loc = find(line_up.begin(),line_up.end(),"Bob");
 	line_up.insert(loc,"Oscar");
 	
-	//Write your code here
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+	
+	line_up.pop_front();
+	line_up.pop_front();
+	
+	loc1 = find(line_up.begin(),line_up.end(),"Sanji");
+	line_up.insert(loc1,"Narutu");
+	
+	line_up.push_front("Prayath");
+	
+	loc2 = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.insert(loc2,"Tony");
+	
+	loc3 = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.erase(loc3);
+	
+	line_up.pop_front();
+	line_up.pop_front();
+	line_up.pop_front();
 	
 	printList(line_up);
 		
